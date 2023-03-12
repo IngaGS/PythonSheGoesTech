7c32d3aa92205e750ea5bbce9e38b0103cdc0ba1
IngaGS <inga.gaudiesiute@gmail.com>
2023-03-12 22:11:25 +0200

    Branching and Loop Tasks

---
+++ Branching&LoopTasks.py
@@ -0,0 +1,75 @@
+# TASK 1
+
+# skaicius = int(input("Write any number : "))
+# if skaicius > 0:
+#     print ("Value is positive")
+# elif skaicius < 0:
+#         print("Value is negative")
+# else:
+#     print ("Value is 0")
+
+# TASK 2
+
+# for i in range (1, 101):
+#     if i%3==0 and i%5==0:
+#         print ("FizzBuzz")
+#     elif i%3==0:
+#         print ("Fizz")
+#     elif i%5==0:
+#         print ("Buzz")
+#     else:
+#         print(i)
+
+# TASK 3
+
+# number = int(input("Enter the number : "))
+#
+# while number < 0:
+#     number = int(input("Number can't be negative, please, enter again : "))
+#
+# print("The factors of " + str(number)+ " are: " )
+#
+# for i in range(1,number + 1):
+#     if number%i==0:
+#         print(i)
+
+# TASK 4
+
+# firstnumber=int(input("Enter first number : "))
+# secondnumber=int(input("Enter second number : "))
+# operator=input("Enter operator : ")
+#
+# # print(type(operator))
+#
+# while not (operator == '*' or operator =='/' or operator == '+' or operator == '-' or operator == '%'):
+#     operator=input("Enter any of these operators *, /, +, -, %: ")
+#
+# if operator == '+':
+#         print("Operation you have entered equals to " + str(firstnumber + secondnumber))
+# elif operator == '-':
+#         print("Operation you have entered equals to " + str(firstnumber - secondnumber))
+# elif operator == '*':
+#         print("Operation you have entered equals to " + str(firstnumber * secondnumber))
+# elif operator == '/':
+#         print("Operation you have entered equals to " + str(firstnumber / secondnumber))
+# elif operator == '%':
+#         print("Operation you have entered equals to " + str(firstnumber % secondnumber))
+
+
+# TASK 5
+
+# number=int(input("Enter an integer : "))
+#
+# while number <= 0:
+#     number = int(input("Number can't be negative, please, enter a positive number : "))
+#
+# for i in range (2, number):
+#     if number%i==0:
+#         print("The number you have entered is not prime")
+#         break
+#     else:
+#         print("The number you have entered is prime")
+#         break
+
+
+
